ff777d8729fef99554a688af8704f93734954c55
mijael450 <molinamijael@gmail.com>
2024-11-22 12:26:43 -0500

    Actualizacion.py

--- main.py
+++ main.py
@@ -6,22 +6,17 @@ v0 = 100      # Depósito inicial
 isem = 8/52      # Tasa de interés semanal (%)
 n = 52        # Número de semanas
 asem = 5      # Aporte semanal
-<<<<<<< HEAD
+
 final = interes.interes()
 
 
-=======
 final = interes.interes(v0,isem,n,asem)
->>>>>>> 943da3e76a203348cd62da525990e8ab4b00d98c
+
 #Devolver de decimal a hexadecimal el espectro RGB
 def RGB_Hexadecimal (rojo:int, verde:int, azul:int): 
     colorRGB="#%02x%02x%02x" % (rojo, verde, azul)
     return colorRGB
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 943da3e76a203348cd62da525990e8ab4b00d98c
 #Ventana Principal del programa
 ventana = tkinter.Tk() 
 #Confirguracion del tamaño de la ventana
@@ -34,17 +29,17 @@ titulo = tkinter.Label(ventana,text="Proyecto 1er Bimestre \n Métodos Numericos
 
 #Declaracion de todos los elementos que se utilizaran para el calculo y presentacion del interes compuesto
 #Contenedor para los elementos de informacion
-<<<<<<< HEAD
+
 cont_Info = tkinter.Frame(ventana)
-=======
+
 cont_Info = tkinter.Frame(ventana,relief="solid")
->>>>>>> 943da3e76a203348cd62da525990e8ab4b00d98c
+
 
 #Aporte interes
 titulo_aporteInicial = tkinter.Label(cont_Info,text="Aporte inicial: ",font=("Arial",13))
 entry_aporteInicial = tkinter.Entry(cont_Info) 
 #Interes anual 
-<<<<<<< HEAD
+
 titulo_interesAnual = tkinter.Label(cont_Info,text="In")
 
 #Colocacion del titulo y contenedor 
@@ -118,4 +113,4 @@ porcentaje.grid(row=2,column=2)
 titulo_aporteSem.grid(row=3,column=0,padx=20)
 entry_aporteSem.grid(row=3,column=1,padx=50)
 ventana.mainloop() 
->>>>>>> 943da3e76a203348cd62da525990e8ab4b00d98c
+
